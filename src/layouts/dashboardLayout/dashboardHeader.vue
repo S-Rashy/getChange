4b7595b8d4422e04071205819fb0493c576c2b90
S-Rashy <sadiqrashidah@gmail.com>
2025-11-04 23:55:04 +0100

    feat: updated layout

--- src/layouts/dashboardLayout/dashboardHeader.vue
+++ src/layouts/dashboardLayout/dashboardHeader.vue
@@ -11,7 +11,7 @@ export default {
     <div class="flex justify-between items-center w-35">
       <div class="w-10 h-10 rounded-[50%] bg-[#6A7E8A]"></div>
       <p class="text-[#013C61] font-medium">Hi, Joshua</p>
-      <svg
+      <!-- <svg
         width="10"
         height="6"
         viewBox="0 0 10 6"
@@ -24,7 +24,7 @@ export default {
           d="M4.99889 5.55556L10 0.00111334L0 0L4.99889 5.55556Z"
           fill="#013C61"
         />
-      </svg>
+      </svg> -->
     </div>
   </main>
 </template>
