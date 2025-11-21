6611863282962677729cc157147e585570000488
S-Rashy <sadiqrashidah@gmail.com>
2025-11-21 23:29:52 +0100

    feat: new updates

--- src/components/slots/Gbutton.vue
+++ src/components/slots/Gbutton.vue
@@ -1,18 +1,13 @@
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <template>
-    <button  class="w-35 h-10 bg-[#2BDA53] hover:bg-[#25C248] hover:shadow-lg hover:scale-105 cursor-pointer text-white transition-all duration-300">
-        <slot></slot>
-
-    </button>
-
+  <button
+    class="w-35 h-10 bg-[#2BDA53] hover:bg-[#25C248] hover:shadow-lg hover:scale-105 cursor-pointer text-white transition-all duration-300"
+  >
+    <slot></slot>
+  </button>
 </template>
 
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/components/slots/Wbutton.vue
+++ src/components/slots/Wbutton.vue
@@ -1,18 +1,13 @@
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <template>
-    <button  class="w-35 h-10 text-[#013C61] border hover:bg-[#013C61] hover:shadow-lg hover:scale-105 hover:text-white cursor-pointer transition-all duration-300">
-        <slot></slot>
-
-    </button>
-
+  <button
+    class="w-35 h-10 text-[#013C61] border hover:bg-[#013C61] hover:shadow-lg hover:scale-105 hover:text-white cursor-pointer transition-all duration-300"
+  >
+    <slot></slot>
+  </button>
 </template>
 
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
